a6e41214b744c68bcd4d43eb01108df62ae38a07
sasha <sasha.mikushova@gmail.com>
2023-12-21 21:11:54 +0200

    Added scheduling

--- JavaPostItNotes/src/main/java/com/example/JavaPostItNotes.java
+++ JavaPostItNotes/src/main/java/com/example/JavaPostItNotes.java
@@ -6,7 +6,6 @@ import com.vaadin.flow.server.PWA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
