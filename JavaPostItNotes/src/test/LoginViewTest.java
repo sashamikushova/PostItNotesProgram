a69767b88577d21ac68a2105b98a71237e837eef
sasha <sasha.mikushova@gmail.com>
2023-12-21 21:17:29 +0200

    deleted unnecessary imports

--- JavaPostItNotes/src/test/LoginViewTest.java
+++ JavaPostItNotes/src/test/LoginViewTest.java
@@ -1,20 +1,12 @@
 import com.example.*;
-import com.vaadin.flow.component.UI;
 
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.textfield.PasswordField;
 import com.vaadin.flow.component.textfield.TextField;
-import com.vaadin.flow.server.Command;
-import com.vaadin.flow.server.VaadinSession;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.data.mongodb.core.MongoTemplate;
-
-import java.awt.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -34,7 +26,6 @@ public class LoginViewTest {
     }
     @Test
     void authenticate_ValidUser_SuccessfulLogin() {
-        User validUser = new User("abc", "abc");
         Span errorMessageField = loginView.getErrorMessageField();
         TextField usernameField = loginView.getUsername();
         PasswordField passwordField = loginView.getPassword();
